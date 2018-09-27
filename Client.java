c0e1d4d3889cc4105fd77e738486cbcef017f583
Matheus <mal3@cin.ufpe.br>
2018-09-27 15:19:00 -0300

    primeiro commit

---
+++ Client.java
@@ -0,0 +1,33 @@
+package att2InfraCom;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.BindException;
+import java.net.Socket;
+
+public class Client {
+
+	public static void main(String[] args) {
+		String address="localhost";
+		try {
+			Socket socket = new Socket(address, 3001);
+
+			DataOutputStream saida = new DatAaOutputStream(socket.getOutputStream());
+
+			int num=10;
+			int num2=20;
+
+			saida.write(num);
+			saida.write(num+num2);
+
+
+			socket.close();
+
+		}catch(BindException e){
+
+		}catch(Exception e){
+
+		}
+
+	}
+
+}
---
+++ server.java
@@ -0,0 +1,33 @@
+package att2InfraCom;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+
+	public static void main(String[] args) {
+
+		try {
+			ServerSocket tmpsocket = new ServerSocket(3001);
+			System.out.println("Aguardando cliente");
+
+			Socket socket = tmpsocket.accept();
+
+			DataInputStream entrada = new DataInputStream(socket.getInputStream());
+
+			System.out.println(entrada.read());
+			System.out.println(entrada.read());
+
+			socket.close();
+
+		}catch(BindException e){
+			System.out.println("Endereço em uso");
+		}catch(Exception e){
+
+		}
+
+	}
+
+}
